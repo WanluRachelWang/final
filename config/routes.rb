a7d504b68b55a3e00031b8d1451f612ffcf8c77f
xuchen <xcthomas1992@gmail.com>
2015-05-07 15:02:53 -0500

    Add Golden 7 for posts and users

--- config/routes.rb
+++ config/routes.rb
@@ -1,56 +1,42 @@
 Rails.application.routes.draw do
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
+  root 'posts#index'
 
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
 
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
+  #########################################################
+  # The "Golden 7" for accessing the "posts" resource
 
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
+  get '/posts/new' => 'posts#new', as: 'new_post'
+  post '/posts' => 'posts#create'
 
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
+  get '/posts' => 'posts#index', as: 'posts'
+  
+  #no page for single post
+  #get '/posts/:id' => 'posts#show', as: 'post'
 
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
+  #posts can not be re-edit and update
+  #get '/posts/:id/edit' => 'posts#edit', as: 'edit_posts'
+  #patch '/posts/:id' => 'movies#posts'
 
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
+  delete '/posts/:id' => 'posts#destroy'
 
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
+  #########################################################
+  # The "Golden 7" for accessing the "users" resource
 
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
+  get '/users/new' => 'users#new', as: 'new_user'
+
+  #no users page, there is only page for single user
+  #get '/users' => 'users#index', as: 'users'
+  
+  #for register
+  post '/users' => 'users#create'
+
+  #index page for a single user
+  get '/users/:id' => 'users#show', as: 'user'
+
+  get '/users/:id/edit' => 'users#edit', as: 'edit_user'
+  patch '/users/:id' => 'users#update'
+
+  get '/users/:id' => 'users#destroy'
+
 end
